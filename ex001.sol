7cd27ce9fe4dd055080058da386f70f7e73fa793
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-06 17:27:43 -0300

    smart contract

---
+++ ex001.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0; //versao: '^' => significa maior que essa consegue abrir
+
+
+contract HelloWorld { // define que é um contrato em seguida o titulo
+    string public name = 'Hello World'; // sem o 'public' o hello world nao aparece em lugar nenhum
+}
+// depois do deploy, na parte inferior esquerda aparece aparece o [name]. Agora é possivel interagir com o contrato, buscar a variavel na blockchain e ela retornar hello world, porque no contrato eu disse que tinha uma variavel 'name' e o valor dela é 'hello world'
