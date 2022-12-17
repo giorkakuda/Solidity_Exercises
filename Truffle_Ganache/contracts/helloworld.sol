1f0fa1bfa126e312d165db56ae59a47b88b75930
giorkakuda <113476281+giorkakuda@users.noreply.github.com>
2022-12-17 11:06:13 -0300

    truffle, ganache, web3

--- Truffle_Ganache/contracts/helloworld.sol
+++ Truffle_Ganache/contracts/helloworld.sol
@@ -1,12 +1,11 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 contract HelloWorld {
-
-    uint public numero = 0;
+    uint public meuNumero = 0;
 
     function setInt (uint _novoNumero) public {
-        numero = _novoNumero;
+        meuNumero = _novoNumero;
     }
 
 }
